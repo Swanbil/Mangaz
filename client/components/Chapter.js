e2571fcdb9594375b36240431988908d641cd069
Swanbil <swan.bilek@bbox.fr>
2023-01-07 11:57:24 +0100

    Feature : Reader manga, swipe left/right

--- client/components/Chapter.js
+++ client/components/Chapter.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import { Icon } from 'react-native-paper';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Dimensions } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { AntDesign } from '@expo/vector-icons';
 import axios from "axios";
 import { API_URL } from '@env';
 import { getDataUser } from '../utilities/localStorage';
+import Carousel from "react-native-reanimated-carousel";
 
 export default function Chapter({ route, navigation }) {
   const [pages, setPages] = useState([]);
   const [selectPageItems, setSelectPageItems] = useState([]);
   const [currentPage, setCurrentPage] = useState({});
   const [numberPageRead, setNumberPageRead] = useState(0);
-  const [isUserHistorySaved, setUserHistorySaved] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isUserHistorySaved, setIsUserHistorySaved] = useState(false);
 
   const { chapterNumber, mangaTitle } = route.params;
+  const NUMBER_PAGE_TO_READ = 10;
 
   useEffect(() => {
     getPagesOfChapter();
@@ -24,40 +26,41 @@ export default function Chapter({ route, navigation }) {
   const getPagesOfChapter = async () => {
     const response = await axios.get(API_URL + `/manga/${mangaTitle}/chapter/${chapterNumber}`);
     setPages(response.data.pages);
-    setSelectPageItems(response.data.pages.map((page, index) => {return {label : 'Page ' + page.pageNumber, value : index }}));
+    setSelectPageItems(response.data.pages.map((page, index) => { return { label: 'Page ' + page.pageNumber, value: index } }));
     setCurrentPage(response.data.pages[0]);
   }
 
-  const saveUserHistoryChapterRead = async () => {
-    const { userPseudo } = await getDataUser();
-    const payload = {
-      userPseudo: userPseudo, //store user pseudo
-      chapterNumber: chapterNumber,
-      mangaName: mangaTitle
-    };
-    console.log("Saving user history", payload);
-    await axios.post(API_URL + `/chapter/history/save`, payload);
-    setUserHistorySaved(true)
+  const isUserHistoryCanBeSaved = () => {
+    if (numberPageRead > NUMBER_PAGE_TO_READ && !isUserHistorySaved) {
+      return true
+    }
+    return false;
   }
 
-  const goPreviousPage = () => {
-    const currentIndex = pages.findIndex((page) => { return page.idPage == currentPage.idPage });
-    if (currentIndex > 0) {
-      setCurrentPage(pages[currentIndex - 1]);
+  const saveUserHistoryChapterRead = async () => {
+    if (isUserHistoryCanBeSaved()) {
+      const { userPseudo } = await getDataUser();
+      const payload = {
+        userPseudo: userPseudo, //store user pseudo
+        chapterNumber: chapterNumber,
+        mangaName: mangaTitle
+      };
+      console.log("Saving user history", payload);
+      await axios.post(API_URL + `/chapter/history/save`, payload);
+      setIsUserHistorySaved(true)
     }
   }
-  const goNextPage = async () => {
-    const currentIndex = pages.findIndex((page) => { return page.idPage == currentPage.idPage });
-    if (currentIndex < pages.length) {
-      setCurrentPage(pages[currentIndex + 1]);
-      setNumberPageRead(state => state + 1);
-    }
-    if (numberPageRead === 8 && !isUserHistorySaved) {    //saving user history if 5 page is read
-      await saveUserHistoryChapterRead();
-    }
-  }
-  const changePange = (value) => {
+
+  const selectPage = (value) => {
+    setCurrentIndex(value)
     setCurrentPage(pages[value]);
+  }
+  const swipePage = (value) => {
+    setCurrentIndex(value)
+    setCurrentPage(pages[value]);
+    if (value > numberPageRead) {
+      setNumberPageRead(prevState => prevState + 1);
+    }
   }
   return (
     <View>
@@ -69,44 +72,49 @@ export default function Chapter({ route, navigation }) {
       </TouchableOpacity>
 
       <View style={styles.container}>
-        <Image source={{ uri: currentPage?.source }} style={{ width: 320, height: 530 }} />
+        <Carousel
+          width={320}
+          height={530}
+          data={pages}
+          scrollAnimationDuration={500}
+          defaultIndex={currentIndex}
+          loop={false}
+          onSnapToItem={(index) => swipePage(index)}
+          onScrollEnd={() => saveUserHistoryChapterRead()}
+          renderItem={({ index }) => (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+              }}
+            >
+              <Image source={{ uri: currentPage?.source }} style={{ width: 320, height: 530 }} />
+            </View>
+          )}
+        />
+
         <View style={{
           flexDirection: "row",
           marginTop: 10,
           alignItems: "center",
         }}>
-          <TouchableOpacity
-            underlayColor='#fff'
-            style={{ marginRight: 80, opacity: ((currentPage?.pageNumber === "1") ? 0.5 : 1) }}
-            onPress={goPreviousPage}
-            disabled={currentPage?.pageNumber === "1"}
-          >
-            <Ionicons name="ios-arrow-back-circle" color="#C0A6F7" size={36} />
-          </TouchableOpacity>
 
           <RNPickerSelect
-            onValueChange={(value) => changePange(value)}
+            onValueChange={(value) => selectPage(value)}
             items={selectPageItems}
+            value={currentIndex}
             placeholder={{ label: "Select page", value: null }}
-            style={{...pickerSelectStyles, iconContainer: {
-              top: 10,
-              right: 12,
-            }}}
+            style={{
+              ...pickerSelectStyles, iconContainer: {
+                top: 10,
+                right: 12,
+              }
+            }}
             useNativeAndroidPickerStyle={false}
             Icon={() => {
               return <Ionicons name="chevron-down-outline" size={16} color="black" />;
             }}
           />
-
-          <TouchableOpacity
-            underlayColor='#fff'
-            style={{ marginLeft: 80, opacity: ((parseInt(currentPage?.pageNumber) === pages.length) ? 0.5 : 1) }}
-            onPress={goNextPage}
-            disabled={(parseInt(currentPage?.pageNumber) === pages.length)}
-          >
-            <Ionicons name="ios-arrow-forward-circle" color="#C0A6F7" size={36} />
-          </TouchableOpacity>
-
         </View>
       </View>
     </View>
@@ -146,8 +154,8 @@ const pickerSelectStyles = StyleSheet.create({
     borderColor: '#DEDEDE',
     borderRadius: 4,
     color: '#B7B7B7',
-    fontStyle:'italic',
-    textAlign:'center',
+    fontStyle: 'italic',
+    textAlign: 'center',
     paddingRight: 30,
   },
   inputAndroid: {
@@ -158,8 +166,8 @@ const pickerSelectStyles = StyleSheet.create({
     borderColor: '#DEDEDE',
     borderRadius: 4,
     color: '#B7B7B7',
-    fontStyle:'italic',
-    textAlign:'center',
+    fontStyle: 'italic',
+    textAlign: 'center',
     paddingRight: 30,
   },
 });
