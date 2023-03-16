551f668e4d97f69917582f4681c8f0f15182d069
ldehonou <61740337+ldehonou@users.noreply.github.com>
2023-03-16 22:57:30 +0100

    Change background

--- client/screens/Web3Home.js
+++ client/screens/Web3Home.js
@@ -235,7 +235,7 @@ export default function Web3Home({ navigation }) {
         <View style={styles.container}>
             <ScrollView>
                 <View>
-                    <ImageBackground source={ require('../assets/anime_cards.jpg') } resizeMode="cover" blurRadius={2} >
+                    <ImageBackground source={ require('../assets/shonen_jump.jpg') } resizeMode="cover" blurRadius={2} >
                         <View style={{ backgroundColor: 'rgba(0,0,0, 0.20)' }}>
                             <View style={{ padding: 20, marginTop: 30 }}>
                                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
