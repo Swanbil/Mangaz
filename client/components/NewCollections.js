19803ab534eb95983262bf1e74afdbaaf8981f85
Sacha BOUDHOU <72568998+SachaBoudhou@users.noreply.github.com>
2023-03-21 23:39:59 +0100

    Add Buy from web3Home

--- client/components/NewCollections.js
+++ client/components/NewCollections.js
@@ -5,6 +5,11 @@ import collectionsData from '../utilities/collections.json';
 
 export default function NewCollections({ navigation, element }) {
 
+    function randomNft(nftList) {
+        const randomIndex = Math.floor(Math.random() * nftList.length);
+        console.log("random ",nftList[randomIndex])
+        return nftList[randomIndex];
+    }
     return (
         <View style={{ flexDirection: 'row', zIndex : 4, marginLeft : 6 }}>
             <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10, marginRight: 37 }}>
@@ -14,7 +19,9 @@ export default function NewCollections({ navigation, element }) {
                     </TouchableOpacity>
                 </View>
                 <View style={{ flexDirection: 'column', marginLeft: 20, top: -55, alignItems: 'flex-start' }}>
-                    <TouchableOpacity style={{ padding: 8, backgroundColor: '#A2B2FC', borderRadius: 15, width: 130, marginLeft : 20, flexDirection : 'row', justifyContent : 'center'}}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('OpenPackScreen', {nft: randomNft(element.nfts)})}
+                        style={{ padding: 8, backgroundColor: '#A2B2FC', borderRadius: 15, width: 130, marginLeft : 20, flexDirection : 'row', justifyContent : 'center'}}>
                         <Text style={{ color: 'white', textAlign: 'center', fontSize: 16, fontWeight: '700', marginLeft : -6 }}>Buy ({element?.pricePack}    )</Text>
                         <Image source={require('../assets/Web3/logoZenCash.png')} style={{ width: 12, height: 12, marginLeft: -18, marginTop : 6  }} />
                     </TouchableOpacity>
